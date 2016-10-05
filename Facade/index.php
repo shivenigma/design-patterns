f6cd5a18bd9b857c12db61b07ccb2d95c343e4c0
Vignesh M <vignesh.m@innoppl.com>
2016-10-05 18:54:54 +0530

    finished implementation of facade design pattern

--- Facade/index.php
+++ Facade/index.php
@@ -5,16 +5,11 @@
  * Date: 05/10/16
  * Time: 6:12 PM
  */
-/**
- * The task is to get the case of a given string inverted.
- * like Vignesh = vIGNESH.
- * The below code do a lot of things to achieve that
- * we can ease this using a facade.
- *
- */
+
+use innoppl\designPatterns\facade\reverseFacade;
+
 $string = "Non Jane Regrette Rien";
-$arrStr = new \innoppl\designPatterns\facade\arrayString();
-$arrRev = new \innoppl\designPatterns\facade\arrayCaseReverse();
-$arrayFromString = $arrStr->stringToArray($string);
-$inverted = $arrRev->reverseCase($arrayFromString);
-$final = $arrStr->arrayToString($inverted);
+
+$final = reverseFacade::getInvertedString($string);
+
+//so by using a facade we abstracted the complex functionality behind a simple elegant API.
--- Facade/reverseFacade.php
+++ Facade/reverseFacade.php
@@ -11,5 +11,12 @@ namespace innoppl\designPatterns\facade;
 
 class reverseFacade
 {
-
+    public static function getInvertedString($string) {
+        $arrStr = new \innoppl\designPatterns\facade\arrayString();
+        $arrRev = new \innoppl\designPatterns\facade\arrayCaseReverse();
+        $arrayFromString = $arrStr->stringToArray($string);
+        $inverted = $arrRev->reverseCase($arrayFromString);
+        $final = $arrStr->arrayToString($inverted);
+        return $final;
+    }
 }
